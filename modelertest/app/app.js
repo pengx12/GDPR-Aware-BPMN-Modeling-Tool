b91c4f5a42d63d2e84f307bdca20bf7137432abb
pengx <pengx@tcd.ie>
2020-06-25 20:07:52 +0100

    update

--- modelertest/app/app.js
+++ modelertest/app/app.js
@@ -137,115 +137,113 @@ function storeRDF(done){
     let baseuri=prompt("Input Base URI");   
     gbaseuri=baseuri
   }
-  let baseuri=gbaseuri
-  let rdfString=document.getElementById("rdf-as-string").value
-  ginsertsqparql=gcsvsqparql+getSparql(rdfString, 'application/rdf+xml',gbaseuri);
+  let baseuri=gbaseuri+'history'
+  let UpdateQuery = `
+  COPY GRAPH <${gbaseuri}>  TO GRAPH <${baseuri}>
+    `;
+    updatesparql(UpdateQuery,function () {
+    let rdfString=document.getElementById("rdf-as-string").value
+    ginsertsqparql=gcsvsqparql+getSparql(rdfString, 'application/rdf+xml',gbaseuri);
 
-	let diagramURI=baseuri
-	var sparqlUpdateQuery = `
-	DROP SILENT GRAPH <${diagramURI}> ;
-	CREATE SILENT GRAPH <${diagramURI}> ;
-	INSERT DATA { 
-		GRAPH <${diagramURI}> {
-			${ginsertsqparql}
-		} 
-	} `;
-	
-	// THIS IS HOW THE QUEYRY LOOKS LIKE.
-	console.log(sparqlUpdateQuery);
-	
-	var request = require('request');
-	request.post(
-		{ url:'http://localhost:3030/ds/update?', form: { update: sparqlUpdateQuery } }, 
-		function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				console.log('successful update');
-        console.log(body);
-        let sparqlqry='PREFIX bpmno: <http://dkm.fbk.eu/index.php/BPMN2_Ontology#> \
-      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
-      Select ?dataobjectref ?datastorereference ?task ?taskname ?dataname ?purpose ?personaldata where {GRAPH <'+gbaseuri+'> {\
-        ?association bpmno:has_targetRef  ?annotation.\
-        ?annotation <http://kdeg.scss.tcd.ie/gdpr-bpmn/ontology#purposeannotation> ?purpose.\
-        ?association bpmno:has_sourceRef  ?datastorereference.\
-        ?datastorereference bpmno:name ?datastorename.\
-        ?dataTable <http://www.w3.org/ns/csvw#columns> ?personaldata .\
-        ?dataTable <http://www.w3.org/ns/csvw#name> ?datastorename.\
-        ?personaldata rdf:type <https://w3id.org/GDPRtEXT/PersonalData> .\
-        ?personaldata <http://www.w3.org/ns/csvw#name> ?dataname.\
-        ?task rdf:type bpmno:task.\
-        ?task bpmno:name ?taskname.\
-        ?task bpmno:has_dataInputAssociation ?datainputass.\
-        ?datainputass bpmno:has_itemAwareElement ?dataobjectref.\
-        ?dataobjectref rdf:type bpmno:dataObjectReference.\
-        ?dataobjectref bpmno:name ?dataname.\
-        }}'
-    request.post(
-      { url:'http://localhost:3030/ds/query?', form: { query: sparqlqry } }, 
-      function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          console.log('successful query');
-          console.log(body);
-          // alert(body)
-          let insersparql=''
-          if (body!=null){
-            let bodyjson=eval('(' + body + ')');
-            let resarr= bodyjson['results']['bindings']
-            let gensparq=''
-            glabelele=new Array()
-            for (let x of resarr){
-              let taskuri=x["task"]["value"]
-              let purpose=x["purpose"]["value"]
-              let datauri=x["personaldata"]["value"]
-
-              glabelele.push(taskuri)
-              glabelele.push(x["dataobjectref"]["value"])
-              glabelele.push(x["datastorereference"]["value"])
-              let hashpurpose=hashCode(purpose)
-              gensparq=gensparq+'<'+datauri+'> <https://w3id.org/GConsent#isPersonalDataForConsent> <'+datauri+'consent>.\n'
-              gensparq=gensparq+'<'+datauri+'consent> <https://w3id.org/GConsent#forPurpose>'+'<http://www.example.org/resource/purpose'+hashpurpose+'>.\n'              
-              gensparq=gensparq+'<http://www.example.org/resource/purpose'+hashpurpose+'>  <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/GConsent#Purpose>.\n'
-              gensparq=gensparq+'<http://www.example.org/resource/purpose'+hashpurpose+'><'+onturi+'purposecontent>\"'+purpose+'\".\n'
-              gensparq=gensparq+'<'+datauri+'consent>  <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/GConsent#Consent>.\n'
-              gensparq=gensparq+'<'+datauri+'> <'+onturi+'isNecessaryFor> <'+taskuri+'>.\n'
-              gensparq=gensparq+'<'+datauri+'consent> <https://w3id.org/GConsent#hasStatus><'+onturi+'ConsentStatusKnowntoRequest>.\n'
-            }
-          let insertsqparql=getSparql(gensparq, 'text/turtle',gbaseuri);
-          done();
-          let sparqlUpdateQuery=`INSERT DATA {  
-            GRAPH <${gbaseuri}> {
-              ${insertsqparql}
-            } 
-          }` ;
-          
-          console.log(sparqlUpdateQuery);
-          var request = require('request');
-          request.post(
-            { url:'http://localhost:3030/ds/update?', form: { update: sparqlUpdateQuery } }, 
-            function (error, response, body) {
-              if (!error && response.statusCode == 200) {
-                console.log('successful update');
-                console.log(body);
-                return selsparql();
-              } else {
-                console.log(response.statusCode)
-                console.warn(error);
+    // let diagramURI=gbaseuri
+    var sparqlUpdateQuery = `
+    DROP SILENT GRAPH <${gbaseuri}> ;
+    CREATE SILENT GRAPH <${gbaseuri}> ;
+    INSERT DATA { 
+      GRAPH <${gbaseuri}> {
+        ${ginsertsqparql}
+      } 
+    } `;
+    updatesparql(sparqlUpdateQuery,function () {
+          let sparqlqry='PREFIX bpmno: <http://dkm.fbk.eu/index.php/BPMN2_Ontology#> \
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
+            Select ?dataobjectref ?datastorereference ?task ?taskname ?dataname ?purpose ?personaldata where {GRAPH <'+gbaseuri+'> {\
+              ?association bpmno:has_targetRef  ?annotation.\
+              ?annotation <http://kdeg.scss.tcd.ie/gdpr-bpmn/ontology#purposeannotation> ?purpose.\
+              ?association bpmno:has_sourceRef  ?datastorereference.\
+              ?datastorereference bpmno:name ?datastorename.\
+              ?dataTable <http://www.w3.org/ns/csvw#columns> ?personaldata .\
+              ?dataTable <http://www.w3.org/ns/csvw#name> ?datastorename.\
+              ?personaldata rdf:type <https://w3id.org/GDPRtEXT/PersonalData> .\
+              ?personaldata <http://www.w3.org/ns/csvw#name> ?dataname.\
+              ?task rdf:type bpmno:task.\
+              ?task bpmno:name ?taskname.\
+              ?task bpmno:has_dataInputAssociation ?datainputass.\
+              ?datainputass bpmno:has_itemAwareElement ?dataobjectref.\
+              ?dataobjectref rdf:type bpmno:dataObjectReference.\
+              ?dataobjectref bpmno:name ?dataname.\
+              }}'
+              querysparql(sparqlqry,function (resarr) {
+              let gensparq=''
+              glabelele=new Array()
+              for (let x of resarr){
+                let taskuri=x["task"]["value"]
+                let purpose=x["purpose"]["value"]
+                let datauri=x["personaldata"]["value"]
+                glabelele.push(taskuri)
+                glabelele.push(x["dataobjectref"]["value"])
+                glabelele.push(x["datastorereference"]["value"])
+                let hashpurpose=hashCode(purpose)
+                gensparq=gensparq+'<'+datauri+'> <https://w3id.org/GConsent#isPersonalDataForConsent> <'+datauri+'consent>.\n'
+                gensparq=gensparq+'<'+datauri+'consent> <https://w3id.org/GConsent#forPurpose>'+'<http://www.example.org/resource/purpose'+hashpurpose+'>.\n'              
+                gensparq=gensparq+'<http://www.example.org/resource/purpose'+hashpurpose+'>  <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/GConsent#Purpose>.\n'
+                gensparq=gensparq+'<http://www.example.org/resource/purpose'+hashpurpose+'><'+onturi+'purposecontent>\"'+purpose+'\".\n'
+                gensparq=gensparq+'<'+datauri+'consent>  <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://w3id.org/GConsent#Consent>.\n'
+                gensparq=gensparq+'<'+datauri+'> <'+onturi+'isNecessaryFor> <'+taskuri+'>.\n'
+                gensparq=gensparq+'<'+datauri+'consent> <https://w3id.org/GConsent#hasStatus><'+onturi+'ConsentStatusKnowntoRequest>.\n'
               }
-            });
+            let insertsqparql=getSparql(gensparq, 'text/turtle',gbaseuri);
+            done();
+            let sparqlUpdateQuery=`INSERT DATA {  
+              GRAPH <${gbaseuri}> {
+                ${insertsqparql}
+              } 
+            }` ;
+            
+            updatesparql(sparqlUpdateQuery,function () {
+              let sparqlqry='PREFIX ont:<'+onturi+'> \
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
+      Select ?dataname ?purposecontent where {GRAPH <'+gbaseuri+'> {\
+        ?consent <https://w3id.org/GConsent#forPurpose> ?purpose.\
+        ?data ont:isNecessaryFor  ?task.\
+        ?data <https://w3id.org/GConsent#isPersonalDataForConsent>  ?consent.\
+        ?purpose ont:purposecontent  ?purposecontent.\
+        ?data <http://www.w3.org/ns/csvw#name> ?dataname.\
+        FILTER( NOT EXISTS {\
+          GRAPH <'+baseuri+'> {\
+            ?consent <https://w3id.org/GConsent#forPurpose> ?purpose.\
+            ?data <https://w3id.org/GConsent#isPersonalDataForConsent>  ?consent.\
+            ?purpose ont:purposecontent  ?purposecontent.\
+            ?data <http://www.w3.org/ns/csvw#name> ?dataname.\
+        }})\
+        }}'
+        querysparql(sparqlqry,function (resarr) {
+          let genstate=''
+          for (let x of resarr){
+            let dataname=x["dataname"]["value"]
+            let purpose=x["purposecontent"]["value"]
+            gloabalsensitivedata+=dataname+' '
+            if (gloabalpurpose.indexOf(purpose)==-1)
+            {
+              gloabalpurpose+=purpose+' '
+            }
+            genstate=genstate+'A new sensitive data '+dataname+' is added. Please notice we need ask for user\'s consent to use their '+dataname+' for '+purpose+' purpose.\n'
           }
-        } else {
-          console.log(response.statusCode)
-          console.warn(error);
-          alert(error)
+          if (genstate!=''){
+            alert(genstate)
+          }
+          selsparql();
+        })
+              // selsparql();
+            })
+              }
+            )
         }
-      });
-			} else {
-				console.log(response.statusCode)
-				console.warn(error);
-			}
-		});
-    // let sparqlqry="PREFIX bpmno: <http://dkm.fbk.eu/index.php/BPMN2_Ontology#> \
-    // PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> SELECT * WHERE {GRAPH <http://example.org/base-uri-for-bmpn-diagram> {?s ?p ?o}}"
-    
+      )
+    })
+	// THIS IS HOW THE QUEYRY LOOKS LIKE.
+	// console.log(sparqlUpdateQuery);
+	 
 }
 function querysparql(sparqlqry,callback){
     var request = require('request');
@@ -262,9 +260,24 @@ function querysparql(sparqlqry,callback){
         } else {
           console.log(response.statusCode)
           console.warn(error);
-          alert(error)
         }
       }});
+}
+
+function updatesparql(sparqlUpdateQuery,callback){
+  var request = require('request');
+  request.post(
+    { url:'http://localhost:3030/ds/update?', form: { update: sparqlUpdateQuery } }, 
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log('successful query');
+        console.log(body);
+        callback ()
+      } else {
+        console.log(response.statusCode)
+        // console.warn(error);
+      }
+    });
 }
 
 function selsparql(callback){
@@ -315,7 +328,6 @@ function selsparql(callback){
         } else {
           console.log(response.statusCode)
           console.warn(error);
-          alert(error)
         }
       }});
 }
@@ -429,7 +441,6 @@ $(function() {
             } else {
               console.log(response.statusCode)
               console.warn(error);
-              alert(error)
             }
           }});
     }
